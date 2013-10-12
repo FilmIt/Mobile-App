8decc23f19ddd58200e57a77a079209d821517b4
Aimen Awan <Aimen Awan@aimen>
2013-10-12 15:12:22 -0700

    EditScreen.

--- gen/com/app/filmit/R.java
+++ gen/com/app/filmit/R.java
@@ -24,16 +24,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int button1=0x7f080008;
+        public static final int action_settings=0x7f080015;
+        public static final int button1=0x7f08000c;
+        public static final int button2=0x7f08000e;
+        public static final int button3=0x7f080006;
+        public static final int button4=0x7f08000a;
+        public static final int button5=0x7f08000f;
+        public static final int button6=0x7f080007;
+        public static final int button7=0x7f08000d;
+        public static final int button8=0x7f080009;
+        public static final int button9=0x7f080008;
         public static final int button_capture=0x7f080002;
         public static final int camera_front_back=0x7f080003;
         public static final int camera_preview=0x7f080000;
-        public static final int capture_select_button=0x7f080007;
+        public static final int capture_select_button=0x7f080011;
         public static final int face_detection=0x7f080004;
-        public static final int gallery_select_button=0x7f080006;
+        public static final int gallery_select_button=0x7f080010;
         public static final int inner_layout=0x7f080001;
-        public static final int text=0x7f080005;
+        public static final int item1=0x7f080012;
+        public static final int item2=0x7f080013;
+        public static final int item3=0x7f080014;
+        public static final int linearLayout1=0x7f08000b;
+        public static final int videoView1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_capture_screen=0x7f030000;
@@ -41,9 +53,10 @@ public final class R {
         public static final int activity_main_menu=0x7f030002;
     }
     public static final class menu {
-        public static final int edit_screen=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int main_menu=0x7f070002;
+        public static final int actions=0x7f070000;
+        public static final int edit_screen=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int main_menu=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
--- src/com/app/filmit/EditScreenActivity.java
+++ src/com/app/filmit/EditScreenActivity.java
@@ -1,30 +1,73 @@
 package com.app.filmit;
 
-
-import android.app.Activity;
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.view.Menu;
-import android.widget.TextView;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.PopupMenu;
+import android.widget.VideoView;
 
 public class EditScreenActivity extends Activity {
+	VideoView videoView;
+	MediaController mediaController;
+	PopupMenu popup;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_edit_screen);
 		
-		Intent intent = getIntent();
-		String address = intent.getStringExtra("path");
-		TextView textView = (TextView) findViewById(R.id.text);
-		textView.setText(address);
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_edit_screen);
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+		
+		playVideo();
+		
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.edit_screen, menu);
 		return true;
+		
+	}
+	
+	@SuppressLint("SdCardPath")
+	void playVideo(){
+		
+		videoView = (VideoView) findViewById(R.id.videoView1);
+		mediaController = new MediaController(this);
+		mediaController.setAnchorView(videoView);
+		
+		Bundle extras = getIntent().getExtras(); 
+		String uri;
+
+		if (extras != null) {
+		    uri = extras.getString("uri");
+		    // and get whatever type user account id is
+		}
+	
+		Uri video = Uri.parse("uri");
+		videoView.setMediaController(mediaController);
+		videoView.setVideoURI(video);
+		
+		videoView.start();
+	}
+	public void showPopup(View v) {
+		
+		popup = new PopupMenu(this, v);
+	    MenuInflater inflater = popup.getMenuInflater();
+	    inflater.inflate(R.menu.actions, popup.getMenu());
+	    popup.show();
 	}
 
 }
