050a447cc6fe83d292f90a2185225723dd085b10
aimenawan <10beseaawan@seecs.edu.pk>
2013-12-06 01:36:11 -0800

    Icons updated

--- gen/com/app/filmit/R.java
+++ gen/com/app/filmit/R.java
@@ -23,11 +23,14 @@ public final class R {
     public static final class drawable {
         public static final int bg31=0x7f020000;
         public static final int chat_check=0x7f020001;
-        public static final int folder_check=0x7f020002;
-        public static final int ic_camera=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int profile_check=0x7f020005;
-        public static final int videocamera=0x7f020006;
+        public static final int chat_icon_final=0x7f020002;
+        public static final int folder_check=0x7f020003;
+        public static final int folder_icon_final=0x7f020004;
+        public static final int ic_camera=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int profile_check=0x7f020007;
+        public static final int profile_icon_final=0x7f020008;
+        public static final int videocamera=0x7f020009;
     }
     public static final class id {
         public static final int action_camera=0x7f08001e;
