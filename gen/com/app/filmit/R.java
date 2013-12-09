1d9af603c12a5ac8b26e3b53237300c3ba658736
aimenawan <10beseaawan@seecs.edu.pk>
2013-12-08 22:33:02 -0800

    updated intro and edit screen

--- gen/com/app/filmit/R.java
+++ gen/com/app/filmit/R.java
@@ -10,6 +10,10 @@ package com.app.filmit;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040000;
+        public static final int transparent_blue=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,8 +21,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int background=0x7f020000;
@@ -28,39 +32,41 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
         public static final int ic_play=0x7f020005;
         public static final int ic_profile=0x7f020006;
+        public static final int ic_save=0x7f020007;
+        public static final int ic_settings=0x7f020008;
+        public static final int ic_undo=0x7f020009;
     }
     public static final class id {
-        public static final int action_camera=0x7f08001c;
-        public static final int action_search=0x7f08001e;
-        public static final int action_settings=0x7f08001d;
-        public static final int button1=0x7f080007;
-        public static final int button2=0x7f08000a;
-        public static final int button3=0x7f08000b;
-        public static final int button4=0x7f08000e;
-        public static final int button6=0x7f08000c;
-        public static final int button8=0x7f08000d;
-        public static final int button9=0x7f080008;
-        public static final int button_capture=0x7f080002;
-        public static final int camera_front_back=0x7f080003;
-        public static final int camera_preview=0x7f080000;
-        public static final int capture_select_button=0x7f080014;
-        public static final int chat_button=0x7f080013;
-        public static final int face_detection=0x7f080004;
-        public static final int folder_button=0x7f080011;
-        public static final int gallery_select_button=0x7f080015;
-        public static final int gridview=0x7f080010;
-        public static final int inner_layout=0x7f080001;
-        public static final int item1=0x7f080019;
-        public static final int item2=0x7f08001a;
-        public static final int item3=0x7f08001b;
-        public static final int linearLayout1=0x7f080006;
-        public static final int name=0x7f080016;
-        public static final int profile_button=0x7f080012;
-        public static final int save_button=0x7f080009;
-        public static final int textView1=0x7f080017;
-        public static final int thumnail=0x7f080018;
-        public static final int videoView1=0x7f080005;
-        public static final int video_capture_button=0x7f08000f;
+        public static final int action_camera=0x7f09001b;
+        public static final int action_search=0x7f09001d;
+        public static final int action_settings=0x7f09001c;
+        public static final int button4=0x7f09000d;
+        public static final int button6=0x7f09000a;
+        public static final int button8=0x7f09000c;
+        public static final int button9=0x7f09000b;
+        public static final int button_capture=0x7f090002;
+        public static final int camera_front_back=0x7f090003;
+        public static final int camera_preview=0x7f090000;
+        public static final int capture_select_button=0x7f090013;
+        public static final int chat_button=0x7f090012;
+        public static final int face_detection=0x7f090004;
+        public static final int folder_button=0x7f090010;
+        public static final int gallery_select_button=0x7f090014;
+        public static final int gridview=0x7f09000f;
+        public static final int inner_layout=0x7f090001;
+        public static final int item1=0x7f090018;
+        public static final int item2=0x7f090019;
+        public static final int item3=0x7f09001a;
+        public static final int linearLayout1=0x7f090006;
+        public static final int name=0x7f090015;
+        public static final int profile_button=0x7f090011;
+        public static final int save_button=0x7f090007;
+        public static final int setting_button=0x7f090009;
+        public static final int textView1=0x7f090016;
+        public static final int thumnail=0x7f090017;
+        public static final int undo_button=0x7f090008;
+        public static final int videoView1=0x7f090005;
+        public static final int video_capture_button=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_capture_screen=0x7f030000;
@@ -72,21 +78,21 @@ public final class R {
         public static final int gridview_element=0x7f030006;
     }
     public static final class menu {
-        public static final int actions=0x7f070000;
-        public static final int edit_screen=0x7f070001;
-        public static final int intro_screen=0x7f070002;
-        public static final int main=0x7f070003;
-        public static final int main_menu=0x7f070004;
-        public static final int video_frames=0x7f070005;
+        public static final int actions=0x7f080000;
+        public static final int edit_screen=0x7f080001;
+        public static final int intro_screen=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int main_menu=0x7f080004;
+        public static final int video_frames=0x7f080005;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_edit_screen=0x7f050003;
-        public static final int title_activity_intro_screen=0x7f050005;
-        public static final int title_activity_main_menu=0x7f050004;
-        public static final int title_activity_video_frames=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_edit_screen=0x7f060003;
+        public static final int title_activity_intro_screen=0x7f060005;
+        public static final int title_activity_main_menu=0x7f060004;
+        public static final int title_activity_video_frames=0x7f060006;
     }
     public static final class style {
         /** 
@@ -110,11 +116,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int Menu_Color=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int Menu_Color=0x7f070002;
     }
 }
