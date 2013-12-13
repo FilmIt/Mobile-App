d3306b74bc277b22f13f2fdc892b774b6be6f111
aimenawan <10beseaawan@seecs.edu.pk>
2013-12-12 19:50:31 -0800

    Editing screen without slider

--- src/com/app/filmit/EditScreenActivity.java
+++ src/com/app/filmit/EditScreenActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.MediaController;
 import android.widget.PopupMenu;
 import android.widget.VideoView;
@@ -39,7 +40,7 @@ public class EditScreenActivity extends Activity {
 	
 
 	private void addButtonListeners() {
-		Button saveButton = (Button) findViewById(R.id.save_button);
+		ImageButton saveButton = (ImageButton) findViewById(R.id.save_button);
 		saveButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				showSaveDialg();
